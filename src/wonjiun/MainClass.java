713bda560c3ec77932391fd6fa8d735d53345aca
wonjiun <sallyjelly2@naver.com>
2021-09-16 10:17:08 +0900

    db 테이블 재생성후 업로드

---
+++ src/wonjiun/MainClass.java
@@ -0,0 +1,29 @@
+package wonjiun;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+//${javafxCompControls},javafx.fxml
+public class MainClass extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("book_wonjiun.fxml"));
+
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		
+		Controller ctl = loader.getController();
+		ctl.setRoot(root);
+		
+		arg0.setScene(scene);
+		arg0.setTitle("도서관리 프로그램");
+		arg0.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
